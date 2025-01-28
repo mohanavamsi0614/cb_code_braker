5774db43b7d56a09429897d0e573aa17badb604e
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-01-28 14:51:06 +0530

    done

--- src/payment.jsx
+++ src/payment.jsx
@@ -4,8 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import api from "./api";
 import Modal from "./Model";
 import done from "/1cbd3594bb5e8d90924a105d4aae924c.gif";
-import qr from "/public/WhatsApp Image 2024-10-25 at 22.50.47_5fab87af.jpg"
-import Nav from './Nav';
+import qr from "/public/WhatsApp Image 2025-01-28 at 13.12.38_f2b94d0a.jpg"
 
 function Payment() {
   const [upiId, setupi] = useState("");
