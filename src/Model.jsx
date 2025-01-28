90610e3a0098053882ed65579dc338ca0119822a
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-01-28 15:40:30 +0530

    done

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,12 +1,16 @@
 // Modal.js
 import React from 'react';
+import Loader from './Loader';
 
 function Modal({ isLoading, children }) {
     return (
         <div className="modal-overlay ">
             <div className="modal-content flex justify-center">
                 {isLoading ? (
-                    <div className="loading-spinner"></div> 
+                    <div className="loading-spinner">
+                        <Loader/>
+                        loading
+                    </div> 
                 ) : (
                     children
                 )}
