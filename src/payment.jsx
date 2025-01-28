da93e24c85eeb6227f8f9a0997d324806ddd898b
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-01-28 20:17:43 +0530

    done

--- src/payment.jsx
+++ src/payment.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import api from "./api";
 import Modal from "./Model";
 import done from "/1cbd3594bb5e8d90924a105d4aae924c.gif";
-import qr from "/public/mo.jpg"
+import qr from "/public/alt4.jpg"
 
 function Payment() {
   const [upiId, setupi] = useState("");
