324c2c87ee6c924de4764d4e7dd9a78b724d0c2e
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-01-29 19:01:34 +0530

    done

--- src/admin.jsx
+++ src/admin.jsx
@@ -42,7 +42,12 @@ function Admin(){
           <h1 className="text-4xl text-white font-bold mb-8 text-center">Admin Dashboard</h1>
           <h1 className=" text-4xl text-white font-bold text-center">Count:{count}</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {teams.map((i)=>(<PaymentCard student={i} key={i.id}/>))}
+
+            {teams.map((i,j)=>(
+              <div key={i._id} className=" flex justify-center items-center">
+              <p className=" text-white font-bold ">{j}. </p>
+              <PaymentCard student={i} key={i.id}/>
+              </div>))}
           </div>
         </div>
       )
